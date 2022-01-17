28f6c12ff4e40a1f52bea873c8326eadfb112ca8
Justin Deal <justin.deal@zapier.com>
2022-01-16 19:55:57 -0600

    Possible first version
    Able to:
    
    - Show path
    - Change position to before or after filename
    - Change font size to small, medium, or large
    - Choose bordered or borderless
    - Add per-path settings to replace specific paths

--- main.ts
+++ main.ts
@@ -1,137 +1,363 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	FileView,
+	Vault,
+	TFolder,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PathTitlePluginSettings {
+	position: string;
+	fontSize: string;
+	borderSize: string;
+	// maxSize: number;
+	// showEllipsis: boolean;
+	pathMappings: Array<Array<string>>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PathTitlePluginSettings = {
+	position: "before",
+	fontSize: "medium",
+	borderSize: "1px",
+	// maxSize: Infinity,
+	// showEllipsis: true,
+	pathMappings: [],
+};
+
+function getFolders(app: App) {
+	const folders: Array<string> = [];
+
+	Vault.recurseChildren(app.vault.getRoot(), (f) => {
+		if (f instanceof TFolder) {
+			folders.push(f.path);
+		}
+	});
+
+	folders.sort();
+
+	return folders;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function arrayToChoices(array: Array<string>) {
+	return array.reduce((result, item) => {
+		result[item] = item;
+		return result;
+	}, {} as Record<string, string>);
+}
+
+export default class PathTitlePlugin extends Plugin {
+	settings: PathTitlePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new PathTitleSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.app.workspace.on("file-open", () => {
+			this.setPaneTitles();
+		});
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+		this.setPaneTitles();
+
+		this.app.vault.on("rename", () => {
+			this.setPaneTitles();
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+	}
+
+	setPaneTitles() {
+		const pathMappings = this.settings.pathMappings.reduce(
+			(result, mapping) => {
+				result[mapping[0]] = mapping[1];
+				return result;
+			},
+			{} as Record<string, string>
+		);
+
+		this.app.workspace.iterateAllLeaves((leaf) => {
+			if (leaf.view instanceof FileView) {
+				const fileView = leaf.view as FileView;
+				const folderPath = fileView.file.parent
+					? fileView.file.parent.path
+					: "";
+				const pathTitle =
+					folderPath in pathMappings
+						? pathMappings[folderPath]
+						: folderPath === "/"
+						? ""
+						: folderPath;
+				// const truncatedPathTitle = this.settings.maxSize
+				// 	? pathTitle.substring(0, this.settings.maxSize)
+				// 	: pathTitle;
+				// const ellipsis =
+				// 	this.settings.showEllipsis &&
+				// 	pathTitle != truncatedPathTitle
+				// 		? "…"
+				// 		: "";
+				// const truncatedPathTitleAndEllipsis =
+				// 	truncatedPathTitle + ellipsis;
+				if (pathTitle) {
+					leaf.view.containerEl.style.setProperty(
+						"--path-title-plugin-border-size",
+						this.settings.borderSize || DEFAULT_SETTINGS.borderSize
+					);
+					leaf.view.containerEl.style.setProperty(
+						"--path-title-plugin-font-size",
+						this.settings.fontSize || DEFAULT_SETTINGS.fontSize
+					);
+					leaf.view.containerEl.addClass(
+						"path-title-plugin-has-path"
+					);
+					if (this.settings.position === "after") {
+						leaf.view.containerEl.style.setProperty(
+							"--path-title-plugin-title-after",
+							`'${pathTitle}'`
+						);
+						leaf.view.containerEl.style.removeProperty(
+							"--path-title-plugin-title-before"
+						);
+					} else {
+						leaf.view.containerEl.style.setProperty(
+							"--path-title-plugin-title-before",
+							`'${pathTitle}'`
+						);
+						leaf.view.containerEl.style.removeProperty(
+							"--path-title-plugin-title-after"
+						);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				} else {
+					leaf.view.containerEl.style.removeProperty(
+						"--path-title-plugin-title-before"
+					);
+					leaf.view.containerEl.style.removeProperty(
+						"--path-title-plugin-title-after"
+					);
+					leaf.view.containerEl.style.removeProperty(
+						"--path-title-plugin-border-size"
+					);
+					leaf.view.containerEl.style.removeProperty(
+						"--path-title-plugin-font-size"
+					);
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		this.app.workspace.iterateAllLeaves((leaf) => {
+			if (leaf.view instanceof FileView) {
+				leaf.view.containerEl.style.removeProperty(
+					"--path-title-plugin-title-before"
+				);
+				leaf.view.containerEl.style.removeProperty(
+					"--path-title-plugin-title-after"
+				);
+				leaf.view.containerEl.removeClass("path-title-plugin-has-path");
+			}
+		});
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+		// if (!this.settings.maxSize) {
+		// 	this.settings.maxSize = Infinity;
+		// }
 	}
 
 	async saveSettings() {
+		console.log(this.settings);
 		await this.saveData(this.settings);
+		this.setPaneTitles();
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class PathTitleSettingTab extends PluginSettingTab {
+	plugin: PathTitlePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PathTitlePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", {}, (el) => {
+			el.setText("Default Settings");
+		});
+
+		let currentSelectedMappingFolder = "";
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Style")
+			.setDesc("How the path looks: position, size, border")
+			.addDropdown((dropdown) => {
+				dropdown
+					.addOptions({
+						before: "Before",
+						after: "After",
+					})
+					.setValue(this.plugin.settings.position)
+					.onChange(async (value) => {
+						this.plugin.settings.position = value;
+						await this.plugin.saveSettings();
+					});
+			})
+			.addDropdown((dropdown) => {
+				dropdown
+					.addOptions({
+						"100%": "Large",
+						"75%": "Medium",
+						"63%": "Small",
+					})
+					.setValue(
+						this.plugin.settings.fontSize ||
+							DEFAULT_SETTINGS.fontSize
+					)
+					.onChange(async (value) => {
+						this.plugin.settings.fontSize = value;
+						await this.plugin.saveSettings();
+					});
+			})
+			.addDropdown((dropdown) => {
+				dropdown
+					.addOptions({
+						"1px": "Border",
+						"0px": "No Border",
+					})
+					.setValue(this.plugin.settings.borderSize)
+					.onChange(async (value) => {
+						this.plugin.settings.borderSize = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		// new Setting(containerEl)
+		// 	.setName("Max Size")
+		// 	.setDesc("Maximum number of characters before truncating")
+		// 	.addText((text) => {
+		// 		text.setValue(
+		// 			this.plugin.settings.maxSize === Infinity ||
+		// 				!this.plugin.settings.maxSize
+		// 				? ""
+		// 				: String(this.plugin.settings.maxSize)
+		// 		).onChange(async (value) => {
+		// 			try {
+		// 				this.plugin.settings.maxSize = Number(value);
+		// 			} catch (e) {
+		// 				this.plugin.settings.maxSize = Infinity;
+		// 			}
+		// 			await this.plugin.saveSettings();
+		// 		});
+		// 	});
+
+		// new Setting(containerEl)
+		// 	.setName("Ellipsis When Truncated")
+		// 	.setDesc(
+		// 		"If Max Size is set, show an ellipsis if path is truncated"
+		// 	)
+		// 	.addToggle((toggle) => {
+		// 		toggle
+		// 			.setValue(this.plugin.settings.showEllipsis)
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.showEllipsis = value;
+		// 				await this.plugin.saveSettings();
+		// 			});
+		// 	});
+
+		containerEl.createEl("h2", {}, (el) => {
+			el.setText("Per Path Settings");
+		});
+
+		new Setting(containerEl)
+			.setName("Add Settings for Path")
+			.setDesc(
+				"Select folder and click add button to add custom settings for a path."
+			)
+			.addDropdown((dropdown) => {
+				dropdown.addOptions(
+					arrayToChoices([""].concat(getFolders(this.plugin.app)))
+				);
+				currentSelectedMappingFolder = dropdown.getValue();
+				dropdown.onChange((value) => {
+					currentSelectedMappingFolder = value;
+				});
+			})
+			.addExtraButton((button) => {
+				button
+					.setIcon("plus-with-circle")
+					.setTooltip("Add Replacement")
+					.onClick(async () => {
+						this.plugin.settings.pathMappings.push([
+							currentSelectedMappingFolder,
+							currentSelectedMappingFolder,
+						]);
+						this.plugin.settings.pathMappings.sort();
+						await this.plugin.saveSettings();
+						this.display();
+					});
+			});
+
+		for (const [
+			index,
+			pathMapping,
+		] of this.plugin.settings.pathMappings.entries()) {
+			let headingEl: HTMLHeadingElement = null;
+			const defaultHeading = "?";
+			containerEl.createEl("h3", {}, (el) => {
+				el.setText(pathMapping[0] || defaultHeading);
+				headingEl = el;
+			});
+
+			new Setting(containerEl)
+				.setName("Original Path")
+				.addText((text) => {
+					text.setValue(pathMapping[0] || "").onChange(
+						async (value) => {
+							this.plugin.settings.pathMappings[index][0] = value;
+							await this.plugin.saveSettings();
+							headingEl.setText(value || defaultHeading);
+						}
+					);
+				});
+
+			new Setting(containerEl)
+				.setName("Replacement Path")
+				.addText((text) => {
+					text.setValue(pathMapping[1] || "").onChange(
+						async (value) => {
+							this.plugin.settings.pathMappings[index][1] = value;
+							await this.plugin.saveSettings();
+						}
+					);
+				});
+
+			new Setting(containerEl)
+				.setName("Remove Path Settings")
+				.addExtraButton((button) => {
+					button
+						.setIcon("trash")
+						.setTooltip("Remove Path Settings")
+						.onClick(async () => {
+							this.plugin.settings.pathMappings.splice(index, 1);
+							this.plugin.settings.pathMappings.sort();
+							await this.plugin.saveSettings();
+							this.display();
+						});
+				});
+		}
 	}
 }
