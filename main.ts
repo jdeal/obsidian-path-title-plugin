e3a015da403ffeeffbb156ca9e0f223a4c23f85b
Justin Deal <justin.deal@zapier.com>
2022-01-25 18:08:40 -0600

    Flexible path replacement pipeline
    Added an ordered list of path replacements with multiple types:
    
    Exact path:
      Matches an exact path and replaces with another path
    Exact folder:
      Matches an exact folder in a path and replaces with another
      folder
    Text:
      Matches a string of text anywhere in path and replaces it with
      some other text
    Regular expression
      Matches a regular expression anywhere in path and replaces the
      matching text with some other text, which can include $1, $2,
      etc. regular expression replacement variables

--- main.ts
+++ main.ts
@@ -14,25 +14,37 @@ import {
 
 // Remember to rename these classes and interfaces!
 
+interface PathSettings {
+	type: string;
+	match: string;
+	replace: string;
+}
+
 interface PathTitlePluginSettings {
 	position: string;
 	fontSize: string;
 	borderSize: string;
+	regexMatch: string;
+	regexReplace: string;
 	// maxSize: number;
 	// showEllipsis: boolean;
 	pathMappings: Array<Array<string>>;
+	pathSettings: Array<PathSettings>;
 }
 
 const DEFAULT_SETTINGS: PathTitlePluginSettings = {
 	position: "before",
 	fontSize: "medium",
 	borderSize: "1px",
+	regexMatch: "",
+	regexReplace: "",
 	// maxSize: Infinity,
 	// showEllipsis: true,
 	pathMappings: [],
+	pathSettings: [],
 };
 
-function getFolders(app: App) {
+function getFolderPaths(app: App) {
 	const folders: Array<string> = [];
 
 	Vault.recurseChildren(app.vault.getRoot(), (f) => {
@@ -46,11 +58,72 @@ function getFolders(app: App) {
 	return folders;
 }
 
+function getAllFolderNames(folderPaths: string[]) {
+	const folderNameSet = folderPaths.reduce((result, path) => {
+		if (path !== "/") {
+			for (const folderName of path.split("/")) {
+				result.add(folderName);
+			}
+		}
+		return result;
+	}, new Set<string>());
+	return Array.from(folderNameSet);
+}
+
 function arrayToChoices(array: Array<string>) {
 	return array.reduce((result, item) => {
 		result[item] = item;
 		return result;
 	}, {} as Record<string, string>);
+}
+
+function getMatch(s: string, re: RegExp) {
+	const match = s.match(re);
+	if (match) {
+		return match[0];
+	}
+	return "";
+}
+
+function escapeForRegExp(text: string) {
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+function applyPathSettings(allPathSettings: Array<PathSettings>, path: string) {
+	return allPathSettings.reduce((currentPath, pathSettings) => {
+		if (pathSettings.match) {
+			if (
+				pathSettings.type === "regexp" ||
+				pathSettings.type === "fuzzy"
+			) {
+				const re = new RegExp(pathSettings.match, "g");
+				return currentPath.replace(re, pathSettings.replace);
+			}
+			if (pathSettings.type === "folder") {
+				const matchFolder = escapeForRegExp(pathSettings.match);
+				const replacementFolder = pathSettings.replace.replace(
+					"$",
+					"$$"
+				);
+				return currentPath.replace(
+					new RegExp(`(^|/)${matchFolder}($|/)`, "g"),
+					`$1${replacementFolder}$2`
+				);
+			}
+			if (pathSettings.type === "exact") {
+				if (currentPath === pathSettings.match) {
+					return pathSettings.replace;
+				}
+			}
+			if (pathSettings.type === "text") {
+				return currentPath.replace(
+					pathSettings.match,
+					pathSettings.replace
+				);
+			}
+		}
+		return currentPath;
+	}, path);
 }
 
 export default class PathTitlePlugin extends Plugin {
@@ -84,30 +157,30 @@ export default class PathTitlePlugin extends Plugin {
 		this.app.workspace.iterateAllLeaves((leaf) => {
 			if (leaf.view instanceof FileView) {
 				const fileView = leaf.view as FileView;
-				const folderPath = fileView.file.parent
+				const path = fileView.file.parent
 					? fileView.file.parent.path
 					: "";
-				const pathTitle =
-					folderPath in pathMappings
-						? pathMappings[folderPath]
-						: folderPath === "/"
-						? ""
-						: folderPath;
-				// const truncatedPathTitle = this.settings.maxSize
-				// 	? pathTitle.substring(0, this.settings.maxSize)
-				// 	: pathTitle;
-				// const ellipsis =
-				// 	this.settings.showEllipsis &&
-				// 	pathTitle != truncatedPathTitle
-				// 		? "…"
-				// 		: "";
-				// const truncatedPathTitleAndEllipsis =
-				// 	truncatedPathTitle + ellipsis;
-				if (pathTitle) {
+				const replacedPath = applyPathSettings(
+					this.settings.pathSettings,
+					path
+				);
+
+				if (replacedPath) {
 					leaf.view.containerEl.style.setProperty(
 						"--path-title-plugin-border-size",
 						this.settings.borderSize || DEFAULT_SETTINGS.borderSize
 					);
+					if (this.settings.borderSize === "0px") {
+						leaf.view.containerEl.style.setProperty(
+							"--path-title-plugin-padding-size",
+							"0px"
+						);
+					} else {
+						leaf.view.containerEl.style.setProperty(
+							"--path-title-plugin-padding-size",
+							"2px"
+						);
+					}
 					leaf.view.containerEl.style.setProperty(
 						"--path-title-plugin-font-size",
 						this.settings.fontSize || DEFAULT_SETTINGS.fontSize
@@ -118,7 +191,7 @@ export default class PathTitlePlugin extends Plugin {
 					if (this.settings.position === "after") {
 						leaf.view.containerEl.style.setProperty(
 							"--path-title-plugin-title-after",
-							`'${pathTitle}'`
+							`'${replacedPath}'`
 						);
 						leaf.view.containerEl.style.removeProperty(
 							"--path-title-plugin-title-before"
@@ -126,7 +199,7 @@ export default class PathTitlePlugin extends Plugin {
 					} else {
 						leaf.view.containerEl.style.setProperty(
 							"--path-title-plugin-title-before",
-							`'${pathTitle}'`
+							`'${replacedPath}'`
 						);
 						leaf.view.containerEl.style.removeProperty(
 							"--path-title-plugin-title-after"
@@ -141,6 +214,9 @@ export default class PathTitlePlugin extends Plugin {
 					);
 					leaf.view.containerEl.style.removeProperty(
 						"--path-title-plugin-border-size"
+					);
+					leaf.view.containerEl.style.removeProperty(
+						"--path-title-plugin-padding-size"
 					);
 					leaf.view.containerEl.style.removeProperty(
 						"--path-title-plugin-font-size"
@@ -176,11 +252,66 @@ export default class PathTitlePlugin extends Plugin {
 	}
 
 	async saveSettings() {
-		console.log(this.settings);
 		await this.saveData(this.settings);
 		this.setPaneTitles();
 	}
 }
+
+function escapeQuotes(s: string) {
+	return s.replace('"', '\\"');
+}
+
+function escapeSlashes(s: string) {
+	return s.replace("/", "\\/");
+}
+
+const replacementTypeToUi: {
+	[key: string]: {
+		heading: (match: string) => string;
+		matchName: string;
+		matchDesc: string;
+		replaceName: string;
+		replaceDesc: string;
+	};
+} = {
+	exact: {
+		heading: (match) =>
+			`Path exactly matches "${escapeSlashes(escapeQuotes(match))}"`,
+		matchName: "Matching Path",
+		matchDesc: "Exact path that will be replaced",
+		replaceName: "Replacement Path",
+		replaceDesc: "Path that will replace matching path",
+	},
+	folder: {
+		heading: (match) =>
+			`Folder in path exactly matches "${escapeQuotes(match)}"`,
+		matchName: "Matching Folder",
+		matchDesc: "Exact folder in path that will be replaced",
+		replaceName: "Replacement Folder",
+		replaceDesc: "Folder that will replace matching folder",
+	},
+	text: {
+		heading: (match) =>
+			`Text anywhere in path matches "${escapeQuotes(match)}"`,
+		matchName: "Matching Text",
+		matchDesc: "Text anywhere in path that will be replaced",
+		replaceName: "Replacement Text",
+		replaceDesc: "Text that will replace matching text",
+	},
+	regexp: {
+		heading: (match) =>
+			`Path matches regular expression /${escapeForRegExp(match).replace(
+				"/",
+				"\\/"
+			)}/`,
+		matchName: "Matching Regular Expression",
+		matchDesc:
+			"Regular expression to match part of path (or full path) that will be replaced",
+		replaceName: "Replacement Text",
+		replaceDesc:
+			"Text that will replace the text that matches the regular expression, can use $1, $2, etc. for groups found in match",
+	},
+};
 
 class PathTitleSettingTab extends PluginSettingTab {
 	plugin: PathTitlePlugin;
@@ -196,14 +327,14 @@ class PathTitleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		containerEl.createEl("h2", {}, (el) => {
-			el.setText("Default Settings");
+			el.setText("General Settings");
 		});
 
-		let currentSelectedMappingFolder = "";
-
 		new Setting(containerEl)
-			.setName("Style")
-			.setDesc("How the path looks: position, size, border")
+			.setName("Path Position")
+			.setDesc(
+				"Where the path is displayed: before or after the filename"
+			)
 			.addDropdown((dropdown) => {
 				dropdown
 					.addOptions({
@@ -215,7 +346,11 @@ class PathTitleSettingTab extends PluginSettingTab {
 						this.plugin.settings.position = value;
 						await this.plugin.saveSettings();
 					});
-			})
+			});
+
+		new Setting(containerEl)
+			.setName("Path Font Size")
+			.setDesc(`Font size of the path`)
 			.addDropdown((dropdown) => {
 				dropdown
 					.addOptions({
@@ -231,7 +366,11 @@ class PathTitleSettingTab extends PluginSettingTab {
 						this.plugin.settings.fontSize = value;
 						await this.plugin.saveSettings();
 					});
-			})
+			});
+
+		new Setting(containerEl)
+			.setName("Path Border")
+			.setDesc(`Whether or not the path has a border`)
 			.addDropdown((dropdown) => {
 				dropdown
 					.addOptions({
@@ -244,6 +383,68 @@ class PathTitleSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					});
 			});
+
+		// new Setting(containerEl)
+		// 	.setName("Folder Replacement")
+		// 	.setDesc(
+		// 		"Provide a regular expression, and each folder will be replaced with what it matches"
+		// 	)
+		// 	.addText((text) => {
+		// 		text.onChange(async (value) => {
+		// 			this.plugin.settings.regexReplace = value;
+		// 			await this.plugin.saveSettings();
+		// 		});
+		// 	});
+
+		/////////////
+
+		// containerEl.createEl("h2", {}, (el) => {
+		// 	el.setText("Default Settings");
+		// });
+
+		// new Setting(containerEl)
+		// 	.setName("Style")
+		// 	.setDesc("How the path looks: position, size, border")
+		// 	.addDropdown((dropdown) => {
+		// 		dropdown
+		// 			.addOptions({
+		// 				before: "Before",
+		// 				after: "After",
+		// 			})
+		// 			.setValue(this.plugin.settings.position)
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.position = value;
+		// 				await this.plugin.saveSettings();
+		// 			});
+		// 	})
+		// 	.addDropdown((dropdown) => {
+		// 		dropdown
+		// 			.addOptions({
+		// 				"100%": "Large",
+		// 				"75%": "Medium",
+		// 				"63%": "Small",
+		// 			})
+		// 			.setValue(
+		// 				this.plugin.settings.fontSize ||
+		// 					DEFAULT_SETTINGS.fontSize
+		// 			)
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.fontSize = value;
+		// 				await this.plugin.saveSettings();
+		// 			});
+		// 	})
+		// 	.addDropdown((dropdown) => {
+		// 		dropdown
+		// 			.addOptions({
+		// 				"1px": "Border",
+		// 				"0px": "No Border",
+		// 			})
+		// 			.setValue(this.plugin.settings.borderSize)
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.borderSize = value;
+		// 				await this.plugin.saveSettings();
+		// 			});
+		// 	});
 
 		// new Setting(containerEl)
 		// 	.setName("Max Size")
@@ -278,18 +479,45 @@ class PathTitleSettingTab extends PluginSettingTab {
 		// 			});
 		// 	});
 
+		// new Setting(containerEl)
+		// 	.setName("Regular Expression Match")
+		// 	.setDesc(
+		// 		"Use a regular expression to match parts of the path to be replaced."
+		// 	)
+		// 	.addText((text) => {
+		// 		text.onChange(async (value) => {
+		// 			this.plugin.settings.regexMatch = value;
+		// 			await this.plugin.saveSettings();
+		// 		});
+		// 	});
+
+		// new Setting(containerEl)
+		// 	.setName("Regular Expression Replace")
+		// 	.setDesc(
+		// 		"If a regular expression is provided, provide the text which replaces the match."
+		// 	)
+		// 	.addText((text) => {
+		// 		text.onChange(async (value) => {
+		// 			this.plugin.settings.regexReplace = value;
+		// 			await this.plugin.saveSettings();
+		// 		});
+		// 	});
+
+		let currentSelectedMappingFolder = "";
+		const regexOption = ":/regex/:";
+
 		containerEl.createEl("h2", {}, (el) => {
-			el.setText("Per Path Settings");
+			el.setText("Path Replacement Settings");
 		});
 
 		new Setting(containerEl)
-			.setName("Add Settings for Path")
+			.setName("Exact Path Replacement")
 			.setDesc(
-				"Select folder and click add button to add custom settings for a path."
+				`Select one of your folders, and click the "+" button to add an exact path replacement for that path below`
 			)
 			.addDropdown((dropdown) => {
 				dropdown.addOptions(
-					arrayToChoices([""].concat(getFolders(this.plugin.app)))
+					arrayToChoices(getFolderPaths(this.plugin.app))
 				);
 				currentSelectedMappingFolder = dropdown.getValue();
 				dropdown.onChange((value) => {
@@ -301,16 +529,256 @@ class PathTitleSettingTab extends PluginSettingTab {
 					.setIcon("plus-with-circle")
 					.setTooltip("Add Replacement")
 					.onClick(async () => {
-						this.plugin.settings.pathMappings.push([
-							currentSelectedMappingFolder,
-							currentSelectedMappingFolder,
-						]);
-						this.plugin.settings.pathMappings.sort();
+						this.plugin.settings.pathSettings.push({
+							type: "exact",
+							match: currentSelectedMappingFolder,
+							replace: currentSelectedMappingFolder,
+						});
 						await this.plugin.saveSettings();
 						this.display();
 					});
 			});
 
+		new Setting(containerEl)
+			.setName("Exact Folder Replacement")
+			.setDesc(
+				`Select one of your folder names, and click the "+" button to add an exact folder replacement for that folder in any path`
+			)
+			.addDropdown((dropdown) => {
+				dropdown.addOptions(
+					arrayToChoices(
+						getAllFolderNames(
+							getFolderPaths(this.plugin.app)
+						).sort()
+					)
+				);
+				currentSelectedMappingFolder = dropdown.getValue();
+				dropdown.onChange((value) => {
+					currentSelectedMappingFolder = value;
+				});
+			})
+			.addExtraButton((button) => {
+				button
+					.setIcon("plus-with-circle")
+					.setTooltip("Add Replacement")
+					.onClick(async () => {
+						this.plugin.settings.pathSettings.push({
+							type: "folder",
+							match: currentSelectedMappingFolder,
+							replace: currentSelectedMappingFolder,
+						});
+						await this.plugin.saveSettings();
+						this.display();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("Other Path or Folder Replacement")
+			.setDesc(
+				"Add a replacement for a different exact path or folder than one listed above"
+			)
+			.addButton((button) => {
+				button.setButtonText("Add Other Path");
+				button.onClick(async () => {
+					this.plugin.settings.pathSettings.push({
+						type: "exact",
+						match: "",
+						replace: "",
+					});
+					await this.plugin.saveSettings();
+					this.display();
+				});
+			})
+			.addButton((button) => {
+				button.setButtonText("Add Other Folder");
+				button.onClick(async () => {
+					this.plugin.settings.pathSettings.push({
+						type: "folder",
+						match: "",
+						replace: "",
+					});
+					await this.plugin.saveSettings();
+					this.display();
+				});
+			});
+
+		new Setting(containerEl)
+			.setName("Text or Regular Expression Replacement")
+			.setDesc(
+				"Add a text or regular expression replacement to match and replace partial text or a regular expression"
+			)
+			.addButton((button) => {
+				button.setButtonText("Add Text");
+				button.onClick(async () => {
+					this.plugin.settings.pathSettings.push({
+						type: "text",
+						match: "",
+						replace: "",
+					});
+					await this.plugin.saveSettings();
+					this.display();
+				});
+			})
+			.addButton((button) => {
+				button.setButtonText("Add Regular Expression");
+				button.onClick(async () => {
+					this.plugin.settings.pathSettings.push({
+						type: "regexp",
+						match: "",
+						replace: "",
+					});
+					await this.plugin.saveSettings();
+					this.display();
+				});
+			});
+
+		for (const [
+			index,
+			pathSettings,
+		] of this.plugin.settings.pathSettings.entries()) {
+			let headingEl: HTMLHeadingElement = null;
+			let matchSetting: Setting = null;
+			let replacementSetting: Setting = null;
+			function setSettingUi(settings: PathSettings) {
+				const emptyMessage = !settings.match
+					? " (will not match any path)"
+					: "";
+				headingEl.setText(
+					`${index + 1}. ${replacementTypeToUi[settings.type].heading(
+						settings.match
+					)}${emptyMessage}`
+				);
+				if (matchSetting && replacementSetting) {
+					matchSetting.setName(
+						replacementTypeToUi[settings.type].matchName
+					);
+					matchSetting.setDesc(
+						replacementTypeToUi[settings.type].matchDesc
+					);
+					replacementSetting.setName(
+						replacementTypeToUi[settings.type].replaceName
+					);
+					replacementSetting.setDesc(
+						replacementTypeToUi[settings.type].replaceDesc
+					);
+				}
+			}
+			containerEl.createEl("h3", {}, (el) => {
+				headingEl = el;
+				setSettingUi(this.plugin.settings.pathSettings[index]);
+			});
+
+			new Setting(containerEl)
+				.setName("Replacement Type")
+				.addDropdown((dropdown) => {
+					dropdown
+						.addOptions({
+							exact: "Exact Path",
+							folder: "Exact Folder in Path",
+							text: "Exact Text in Path",
+							regexp: "Regular Expression in Path",
+						})
+						.setValue(
+							this.plugin.settings.pathSettings[index].type ===
+								"fuzzy"
+								? "regexp"
+								: this.plugin.settings.pathSettings[index].type
+						)
+						.onChange((value) => {
+							this.plugin.settings.pathSettings[index].type =
+								value;
+							setSettingUi(
+								this.plugin.settings.pathSettings[index]
+							);
+							this.plugin.saveSettings();
+						});
+				});
+
+			matchSetting = new Setting(containerEl).addText((text) => {
+				text.setValue(pathSettings.match || "").onChange(
+					async (value) => {
+						this.plugin.settings.pathSettings[index].match = value;
+						await this.plugin.saveSettings();
+						setSettingUi(this.plugin.settings.pathSettings[index]);
+					}
+				);
+			});
+
+			replacementSetting = new Setting(containerEl).addText((text) => {
+				text.setValue(pathSettings.replace || "").onChange(
+					async (value) => {
+						this.plugin.settings.pathSettings[index].replace =
+							value;
+						await this.plugin.saveSettings();
+					}
+				);
+			});
+
+			setSettingUi(this.plugin.settings.pathSettings[index]);
+
+			new Setting(containerEl)
+				//.setName("Remove Path Settings")
+				.addExtraButton((button) => {
+					button
+						.setIcon("trash")
+						.setTooltip("Remove replacement")
+						.onClick(async () => {
+							this.plugin.settings.pathSettings.splice(index, 1);
+							await this.plugin.saveSettings();
+							this.display();
+						});
+				})
+				.addExtraButton((button) => {
+					button
+						.setIcon("down-arrow-with-tail")
+						.setTooltip(
+							index >=
+								this.plugin.settings.pathSettings.length - 1
+								? "Already last"
+								: "Move replacement down"
+						)
+						.setDisabled(
+							index >=
+								this.plugin.settings.pathSettings.length - 1
+						)
+						.onClick(async () => {
+							const pathSettings =
+								this.plugin.settings.pathSettings[index];
+							this.plugin.settings.pathSettings.splice(index, 1);
+							this.plugin.settings.pathSettings.splice(
+								index + 1,
+								0,
+								pathSettings
+							);
+							await this.plugin.saveSettings();
+							this.display();
+						});
+				})
+				.addExtraButton((button) => {
+					button
+						.setIcon("up-arrow-with-tail")
+						.setTooltip(
+							index === 0
+								? "Already first"
+								: "Move replacement up"
+						)
+						.setDisabled(index === 0)
+						.onClick(async () => {
+							const pathSettings =
+								this.plugin.settings.pathSettings[index];
+							this.plugin.settings.pathSettings.splice(index, 1);
+							this.plugin.settings.pathSettings.splice(
+								index - 1,
+								0,
+								pathSettings
+							);
+							await this.plugin.saveSettings();
+							this.display();
+						});
+				});
+		}
+
+		/*
 		for (const [
 			index,
 			pathMapping,
@@ -359,5 +827,6 @@ class PathTitleSettingTab extends PluginSettingTab {
 						});
 				});
 		}
+*/
 	}
 }
